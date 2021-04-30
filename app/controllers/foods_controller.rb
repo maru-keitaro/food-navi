b749778ae1ef90e7e9757fd7512d6d840995b500
maru-keitaro <maru.keitaro@gmail.com>
2021-04-30 22:51:22 +0900

    投稿一覧

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,4 +1,18 @@
 class FoodsController < ApplicationController
   def index
   end
+
+  def new
+    @food = Food.new
+  end
+
+  def create
+  end
+
+  private
+  
+  def food_params
+    params.require(:food).permit(:store_name, :content, :image).merge(user_id: current_user.id)
+  end
+  
 end
